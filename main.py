5377dbc2c47868f25d02f1040287c043f363e4db
paulovictor181 <paulo_victor_cc82@outlook.com>
2024-12-11 01:48:12 -0300

    Feat: Criação de compilador, Feito o Lexer, interface grafica simples e readme

---
+++ main.py
@@ -0,0 +1,72 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from lexer import build_lexer
+
+
+def read_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        messagebox.showerror("Erro", f"O arquivo '{file_name}' não foi encontrado.")
+        return None
+
+
+def process_file():
+    file_name = filedialog.askopenfilename(
+        title="Selecione um arquivo",
+        filetypes=(("Arquivos de texto", "*.txt"))
+    )
+    if not file_name:
+        return
+
+    code = read_file(file_name)
+    if code is None:
+        return
+
+    lexer = build_lexer()
+    lexer.input(code)
+
+    tokens = []
+    for token in lexer:
+        tokens.append(f"Token({token.type}, '{token.value}', Linha {token.lineno})")
+
+    # Exibe os tokens encontrados
+    resultado_tokens.delete(1.0, tk.END)
+    resultado_tokens.insert(tk.END, "\n".join(tokens))
+
+    # Exibe os erros encontrados
+    resultado_erros.delete(1.0, tk.END)
+    if lexer.errors:
+        resultado_erros.insert(tk.END, "\n".join(lexer.errors))
+    else:
+        resultado_erros.insert(tk.END, "Nenhum erro encontrado.")
+
+    lexer.errors.clear()
+
+
+
+
+
+
+
+
+
+# Criar a janela principal
+janela = tk.Tk()
+janela.title("Processador de Tokens")
+
+# Botão para selecionar o arquivo
+botao_selecionar = tk.Button(janela, text="Selecionar Arquivo", command=process_file)
+botao_selecionar.pack(pady=10)
+
+# Área de texto para exibir os tokens
+resultado_tokens = tk.Text(janela, wrap=tk.WORD, height=10, width=60)
+resultado_tokens.pack(padx=10, pady=10)
+
+# Área de texto para exibir os erros
+resultado_erros = tk.Text(janela, wrap=tk.WORD, height=10, width=60)
+resultado_erros.pack(padx=10, pady=10)
+
+# Executar a aplicação
+janela.mainloop()
