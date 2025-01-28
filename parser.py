a5aaaf9cf7083074379f91163d38bc09bb66ca03
paulovictor181 <paulo_victor_cc82@outlook.com>
2025-01-27 23:01:41 -0300

    Refatorando tudo

--- parser.py
+++ parser.py
@@ -16,7 +16,7 @@ def p_classes(p):
         p[0] = [p[1]] + p[2]  # Adiciona a nova classe à lista de classes
 
 def p_primitive_class(p):
-    '''primitive_class : CLASS CLASS_IDENTIFIER subclass_section closure_axiom disjoint_section individuals_section'''
+    '''primitive_class : CLASS CLASS_IDENTIFIER subclass_section disjoint_section individuals_section'''
     p[0] = {
         "type": "primitive_class",
         "name": p[2],
@@ -26,12 +26,12 @@ def p_primitive_class(p):
     }
 
 def p_subclass_section(p):
-    '''subclass_section : SUBCLASSOF def_descriptions
-                        | SUBCLASSOF enum_class
-                        | SUBCLASSOF covered_class
+    '''subclass_section : SUBCLASSOF CLASS_IDENTIFIER def_descriptions
+                        | SUBCLASSOF CLASS_IDENTIFIER enum_class
+                        | SUBCLASSOF CLASS_IDENTIFIER OR covered_class
                         | SUBCLASSOF CLASS_IDENTIFIER 
-                        | empty'''
-    if len(p) == 3:
+                        '''
+    if len(p) == 2:
         p[0] = p[2]  
     else:
         p[0] = []  
@@ -55,9 +55,9 @@ def p_disjoint_classes_list(p):
 
 def p_defined_class(p):
     '''defined_class : CLASS CLASS_IDENTIFIER equivalentto_section subclass_section individuals_section
-                       | CLASS CLASS_IDENTIFIER equivalentto_section'''
+                     | CLASS CLASS_IDENTIFIER equivalentto_section'''
 
-    if len(p) == 6:
+    if len(p) == 5:
         p[0] = {
             "type": "defined_class",
             "name": p[2],
@@ -111,9 +111,11 @@ def p_covered_class(p):
             "type": "covered_class",
             "Classe": p[1],
         }
-def p_closure_axiom(p):
-    '''closure_axiom : ONLY OPEN_PAREN def_descriptions CLOSE_PAREN'''
-    p[0] = ('closure_axiom', p[1], p[3])  
+    print('cheguei aqui')
+
+# def p_closure_axiom(p):
+#    '''closure_axiom : ONLY OPEN_PAREN def_descriptions CLOSE_PAREN'''
+#    p[0] = ('closure_axiom', p[1], p[3])  
 
 
 def p_aux_fechamento(p):
@@ -141,41 +143,41 @@ def p_aux_fechamento(p):
 
 
 
-def p_ani_abertura(p):
-    '''ani_abertura : comma_and OPEN_PAREN def_descriptions CLOSE_PAREN quantifier ani_abertura
-                    | comma_and OPEN_PAREN def_descriptions CLOSE_PAREN'''
-    if len(p) == 3:
-        p[0] = ('ani_abertura', p[2])
-    else:
-        p[0] = ('ani_abertura', p[2], p[4], p[5] )
+#def p_ani_abertura(p):
+#    '''ani_abertura : comma_and OPEN_PAREN def_descriptions CLOSE_PAREN quantifier ani_abertura
+#                    | comma_and OPEN_PAREN def_descriptions CLOSE_PAREN'''
+#    if len(p) == 3:
+#        p[0] = ('ani_abertura', p[2])
+#    else:
+#        p[0] = ('ani_abertura', p[2], p[4], p[5] )
 
-
-def p_aninhada(p):
-    '''aninhada : CLASS_IDENTIFIER ani_abertura
-           '''
-    if len(p) == 6:
-        p[0] = ('aninhada', p[1], p[3], p[4], p[5])
-    else:
-        p[0] = ('aninhada', p[1], p[2], p[3])
 
 
 def p_equivalentto_section(p):
-    '''equivalentto_section : EQUIVALENTTO CLASS_IDENTIFIER comma_and def_descriptions
-                            | EQUIVALENTTO enum_class
-                            | EQUIVALENTTO covered_class
-                            | EQUIVALENTTO aninhada
+    '''equivalentto_section : EQUIVALENTTO enum_class
+                            | EQUIVALENTTO CLASS_IDENTIFIER OR covered_class
+                            | EQUIVALENTTO CLASS_IDENTIFIER comma_and def_descriptions
     '''
-    p[0] = p[2]
-
+    if len(p) == 4: 
+        p[0] =  p[4]
+    else: 
+        p[0] = p[2]
 
 def p_def_descriptions(p):
-    '''def_descriptions : class_aux
+    '''def_descriptions : aninhada
+                        | aninhada def_descriptions
+                        | class_aux
                         | class_aux def_descriptions 
                         | quantifier_aux
                         | quantifier_aux def_descriptions
     '''  
     p[0] = p[1]
+    print('description')
 
+def p_aninhada(p):
+    '''aninhada :  '''
+
+    p[0] = ('aninhada', p[1], p[2], p[4])
 
 def p_class_aux(p):
     '''class_aux : CLASS_IDENTIFIER
@@ -186,18 +188,29 @@ def p_class_aux(p):
     p[0] = {
         'class': p[1]
     }
+    print('class_aux')
 
 def p_quantifier_aux(p):
     '''quantifier_aux : PROPERTY_IDENTIFIER quantifier CLASS_IDENTIFIER
                       | PROPERTY_IDENTIFIER quantifier namespace_type
                       | OPEN_PAREN quantifier_aux CLOSE_PAREN
+                      | OPEN_PAREN aninhada CLOSE_PAREN
                       | quantifier_aux comma_and quantifier_aux
+                      | PROPERTY_IDENTIFIER quantifier quantifier_aux
+                      | CLASS_IDENTIFIER quantifier quantifier_aux
+                      | CLASS_IDENTIFIER OR quantifier_aux
+                      | CLASS_IDENTIFIER comma_and quantifier_aux
+                      | CLASS_IDENTIFIER
     '''
     p[0] = {
-        'property': p[1],
-        'quantifier': p[2],
-        'target': p[3]
+
     }
+    print('quantifier_aux');
+    
+
+
+
+
 
 # Comparação
 def p_quantifier(p):
